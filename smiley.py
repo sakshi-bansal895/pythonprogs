623564d78802ca9a62b47737489b11ff30b975a9
sakshi-bansal895 <sakshi81323@yahoo.com>
2025-11-14 13:37:04 +0530

    Add files via upload

---
+++ smiley.py
@@ -0,0 +1,20 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("\U0001F600")
+😀
+>>> print("\U0001F600")*200
+😀
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print("\U0001F600")*200
+TypeError: unsupported operand type(s) for *: 'NoneType' and 'int'
+>>> x=5
+>>> print("x")
+x
+>>> x=5
+>>> print (x)
+5
+>>> print("\U0001F600*200")
+😀*200
+>>> print("\U0001F600" *200)
+😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀
